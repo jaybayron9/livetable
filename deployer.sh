d54bee3a23075036ed1f43b5fbfdc551390ddbb2
jaybayron9 <82939971+jaybayron9@users.noreply.github.com>
2023-12-05 01:54:49 +0800

    changes to cd/cd

--- deployer.sh
+++ deployer.sh
@@ -2,8 +2,8 @@ set -e
 
 echo "Deploying application ..."
 
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute)
-    git pull origin master
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute')
+git pull origin master
 
 php artisan up
 
